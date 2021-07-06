3af68b03944f1d3c37865ee26dd94c243fc88a6b
Ilhanalim <ilhanalim46@gmail.com>
2021-07-06 12:49:06 +0700

    changes button color

---
+++ script.js
@@ -0,0 +1,148 @@
+const quizData = [
+    {
+        question: 'How old am I?',
+        a: '15',
+        b: '17',
+        c: '18',
+        d: '20',
+        answer: 'c'
+    }, {
+        question: 'Favourite Anime?',
+        a: 'Death Note',
+        b: 'Sword Art Online',
+        c: 'Gintama',
+        d: 'Kimi no Nawa',
+        answer: 'a'
+    }, {
+        question: 'Favourite Band?',
+        a: 'Nirvana',
+        b: 'AC/DC',
+        c: 'Red Hot Chili Peppers',
+        d: 'Guns n Roses',
+        answer: 'c'
+    }, {
+        question: 'How to read "恋愛映画"?',
+        a: 'れんあうえいが',
+        b: 'れんあうえいか',
+        c: 'れんあいえいか',
+        d: 'れんあいえいが',
+        answer: 'd'
+    }, {
+        question: '1 + 1 x 0?',
+        a: '0',
+        b: '1',
+        c: '2',
+        d: '3',
+        answer: 'b'
+    }
+]
+
+
+const noQuestion = document.getElementById('noques');
+const question = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const btnSubmit = document.getElementById('btnSubmit');
+const btnTry = document.querySelector('.btn-try');
+const scoreEl = document.querySelector('.score');
+const ans = document.querySelectorAll('.answer');
+// const ansArr = Array.from(ans);
+
+var currentQuizData = 0;
+var noQues = 1;
+var score = 0;
+var isChecked = true;
+
+
+loadQuiz();
+
+function loadQuiz() {
+    let currentQuiz = quizData[currentQuizData];
+    // show data to html
+    noQuestion.innerText = noQues;
+    question.innerText = currentQuiz.question;
+    a_text.innerText = currentQuiz.a;
+    b_text.innerText = currentQuiz.b;
+    c_text.innerText = currentQuiz.c;
+    d_text.innerText = currentQuiz.d;
+    
+}
+
+
+function getAnswer() {
+    let currentQuiz = quizData[currentQuizData];
+    // console.log(currentQuiz);
+    for (let e of ans) {
+        if (e.checked) {
+            console.log(e.id);
+            if (e.id === currentQuiz.answer) {
+                console.log('benar');
+                score++;
+            } else {
+                console.log("salah");
+            }
+            e.checked = false;
+            isChecked = true;
+            break;
+        } else {
+            isChecked = false;
+        }
+    }
+    // Tidak bisa pakai break 
+    // ans.forEach(e => {
+    //     if (e.checked) {
+    //         console.log(e.id);
+    //         if (e.id === currentQuiz.answer) {
+    //             console.log('benar');
+    //             score++;
+    //         } else {
+    //             console.log("salah");
+    //         }
+    //         e.checked = false;
+    //         isChecked = true;
+    //         break;
+    //         console.log(isChecked);
+    //     } else {
+    //         isChecked = false;
+    //     }
+    // });
+    // console.log(isChecked);
+}
+
+btnSubmit.addEventListener('click', () => {
+
+    getAnswer();
+    if (isChecked) {
+        if (currentQuizData < (quizData.length - 1)) {
+            console.log(currentQuizData);
+            currentQuizData++;
+            noQues++;
+            loadQuiz(); 
+        } else {
+            btnSubmit.setAttribute('data-target', '#gameover');
+            scoreEl.innerText = `${score} / ${quizData.length}`;
+            btnTry.addEventListener('click', () => {
+                location.reload();
+            });
+        }
+    }     
+    
+});
+
+
+// function getAnswer() {
+//     let currentQuiz = quizData[currentQuizData];
+//     ans.forEach(e => {
+//         // console.log(e.checked);
+//         if (e.checked) {
+//             if (e.id === currentQuiz.answer) {
+//                 console.log('haha');
+//                 score++;
+//                 return true;
+//             }
+//         }
+//     });
+//     return false;
+// }
